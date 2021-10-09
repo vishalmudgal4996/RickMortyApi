4021380f741446e49694752d97fb9ce34e9d4e60
Vishal Mudgal <vishalmudgal4996@gmail.com>
2021-10-09 14:55:39 +0530

    hash controller added

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "../screens/home/Home";
 import Profile from './profile/Profile';
 
@@ -15,11 +15,11 @@ class Controller extends Component {
         <div className="main-container">
           <Route
             exact
-            path="/RickMortyApi"
+            path={"/RickMortyApi/"} 
             render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
           />
           {/* <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} /> */}
-          <Route path='/RickMortyApi/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+          <Route path={'/RickMortyApi/profile'} render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
           {/* <Route path="/" exact {...props} component={HomePage} /> */}
           {/* <Route path="/chars" exact {...props} component={CharList} /> */}
           {/* <Route path="/chars/:id" {...props} component={SoloChar} /> */}
